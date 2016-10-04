ec5884dc876222369c82b97f0cacad38e63230e7
LingboTang <lingbo19.tang@gmail.com>
2016-10-04 01:28:36 -0600

    Using struct now

--- Asn1/passingStruct.c
+++ Asn1/passingStruct.c
@@ -13,18 +13,20 @@
 #define X 20
 #define THREAD_COUNT 3
 
+
 pthread_barrier_t mybarrier;
 
-struct threadData{
-	long int **data;
-	long int **sample;
-
-};
+typedef struct threadData{
+	long int *chunk;
+	long int *sample;
+	int pid;
+	int n;
+	int ps;
+} thrD;
 
 int cmpfunc(const void*a, const void*b);
 void printArr(long int* arr, int size);
-void* threadFn(void * chunk);
-
+void* threadFn(void * chunkD);
 
 int main(int argc, char**argv) {
 	if (argc != 3)
@@ -34,10 +36,20 @@ int main(int argc, char**argv) {
 	}
 	int numbers = atoi(argv[1]);
 	int ps = atoi(argv[2]);
-	long int * array = (long int *) malloc(sizeof(long int)*N);
 	int i;
+	thrD *mythrD = (thrD *) malloc(sizeof(thrD)*ps);
+	for (i = 0; i < ps; i++)
+	{
+		mythrD[i].pid = i;
+		mythrD[i].n = numbers;
+		mythrD[i].ps = ps;
+ 	}
+
+	
+	long int * array = (long int *) malloc(sizeof(long int)*N);
+	
 	pthread_t ids[THREAD_COUNT];
-	//int short_ids[THREAD_COUNT];
+
 	srandom(time(NULL));
 	
 	pthread_barrier_init(&mybarrier, NULL, THREAD_COUNT + 1);
@@ -54,17 +66,17 @@ int main(int argc, char**argv) {
 	{
 		data[i] = (long int*) malloc(sizeof(long int)*chunkSize);
 		memcpy(data[i],&array[i*chunkSize], chunkSize*sizeof(long int));
+		mythrD[i].chunk = data[i];
 	}	
 	
 	printf("Before Sorting: \n");
 	for (i =0; i < THREAD_COUNT; i++)
 	{
-		printArr(data[i],12);
+		printArr(mythrD[i].chunk, 12);
 	}
 
 	for (i=0; i < THREAD_COUNT; i++) {
-		//short_ids[i] = i;
-        pthread_create(&ids[i], NULL, threadFn, (void*)data[i]);
+        pthread_create(&ids[i], NULL, threadFn, (void*)&(mythrD[i]));
     }
 
 
@@ -77,16 +89,11 @@ int main(int argc, char**argv) {
 	pthread_barrier_destroy(&mybarrier);
 
 	
-	//clock_t begin = clock();
-	//qsort(array,N,sizeof(long int),cmpfunc);
-	//clock_t end = clock();
-	//double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
 	printf("After Sorting: \n");
 	for (i =0; i < THREAD_COUNT; i++)
 	{
-		printArr(data[i],12);
+		printArr(mythrD[i].chunk, 12);
 	}
-	//printf("time spent: %f\n",time_spent);
 
 	free((void *) array);
 	for (i = 0; i < THREAD_COUNT; i++)
@@ -94,14 +101,22 @@ int main(int argc, char**argv) {
 		free((void*)data[i]);
 	}
 	free((void *) data);
+	
 
 
     return 0;
 }
 
-int cmpfunc(const void*a, const void*b)
-{
-	return (*(long int*)a - *(long int*)b);
+void* threadFn(void * chunkD) {
+	thrD mychunkD = *(thrD *) chunkD;
+	int off_set = mychunkD.n/(mychunkD.ps*mychunkD.ps);
+	int chunkSize = mychunkD.n/mychunkD.ps;
+	int wait_sec = 1 + rand() % 2;
+	sleep(wait_sec);
+	pthread_barrier_wait(&mybarrier);
+	qsort(mychunkD.chunk,chunkSize,sizeof(long int),cmpfunc);
+	sleep(wait_sec);
+	return NULL;
 }
 
 void printArr(long int* arr, int size)
@@ -121,13 +136,8 @@ void printArr(long int* arr, int size)
 	}
 }
 
-void* threadFn(void * chunk) {
-	long int *thisChunk = (long int *)chunk;
-	int w = 36/(3*3);
-	int wait_sec = 1 + rand() % 2;
-	//sleep(wait_sec);
-	pthread_barrier_wait(&mybarrier);
-	qsort(thisChunk,12,sizeof(long int),cmpfunc);
-	sleep(wait_sec);
-	return NULL;
+int cmpfunc(const void*a, const void*b)
+{
+	return (*(long int*)a - *(long int*)b);
 }
+
