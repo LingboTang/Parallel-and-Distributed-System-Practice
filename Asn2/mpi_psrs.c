4d0ac62ce198cacc89ff67f7119ff54c07dc0fa4
LingboTang <lingbo19.tang@gmail.com>
2016-10-25 21:22:35 -0600

    Founded how to scatter it

--- Asn2/mpi_psrs.c
+++ Asn2/mpi_psrs.c
@@ -2,10 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <pthread.h>
 #include <time.h>
 #include "mpi.h"
 
 #define MASTER 0
+#define MYSEED 23478237
 
 void printArr(long int* arr, int size)
 {
@@ -24,6 +26,12 @@ void printArr(long int* arr, int size)
     }
 }
 
+int cmpfunc(const void*a, const void*b)
+{
+    return (*(long int*)a - *(long int*)b);
+}
+
+
 void phase1() 
 {
 
@@ -32,12 +40,13 @@ void phase1()
 int main(int argc, char *argv[])
 {
 
-    MPI_Init(&argc, &argv);
+    
     /* Global Variables */
     int N, thrN, taskid;
     char * outputFilename;
     FILE * outfp;
 
+    printf("How many arguments?: %d\n",argc);
     /* N, P and output get from Command line */
     if (argc < 4)
     {
@@ -51,27 +60,42 @@ int main(int argc, char *argv[])
 
     /* Init the test array*/
     long int * randArray = (long int *) malloc(sizeof(long int)*N);
-    long int * subRand = (long int *) malloc(sizeof(long int) * N/thrN);
-    srandom(time(NULL));
+    long int * subRand = (long int *) malloc(sizeof(long int)*N/thrN);
+    srandom(MYSEED);
 
     for (int i = 0; i < N; i++)
     {
         randArray[i] = random()%36;
     }
 
-    printArr(randArray,N);
-
+    MPI_Init(&argc, &argv);
     
-    /* Start */
     MPI_Comm_size(MPI_COMM_WORLD, &thrN);
     MPI_Comm_rank(MPI_COMM_WORLD,&taskid);
 
-    if (taskid == MASTER) {
-        MPI_SCatter(randArray, N/thrN,MPI_LONG,subRand, N/thrN, MPI_LONG, MASTER, MPI_COMM_WORLD);
-    }    
+    printArr(randArray, N);
+    printf("This is Length of Origin Array: %d\n", thrN);
+    
+    /* Start */
+    printf("Before iter\n");
+    for (int i = 0; i < thrN - 1; i++)
+    {
+        printf("In iter\n");
 
-    printArr(subRand,N/thrN);
+        memset(subRand, 0, N/thrN * sizeof(long int));
+        if (taskid == MASTER) {
+            MPI_Scatter(randArray, N/thrN, MPI_LONG, subRand, N/thrN, MPI_LONG, MASTER, MPI_COMM_WORLD);
+        }
 
+        qsort(subRand, N/thrN, sizeof(long int), cmpfunc);    
+        printf("taskid: %d \n", taskid);
+        printArr(subRand, N/thrN);
+
+    }
+    printf("After iter\n");
+
+
+    MPI_Finalize();    
     free(randArray);
     free(subRand);
     fclose(outfp);
