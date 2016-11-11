7fb1d5f76a89c823bf9af852def7fc7032c5f337
LingboTang <lingbo19.tang@gmail.com>
2016-11-11 00:31:14 -0700

    Here is mine

--- Asn2/my_psrs.c
+++ Asn2/my_psrs.c
@@ -99,6 +99,22 @@ int isSorted(long int * arr, int size) {
     return 1;
 }
 
+int binarySearch(int *arr, int l, int r, int x)
+{
+   if (r >= l)
+   {
+        int mid = l + (r - l)/2;
+
+        if (arr[mid] == x)  return mid;
+
+        if (arr[mid] > x) return binarySearch(arr, l, mid-1, x);
+
+        return binarySearch(arr, mid+1, r, x);
+   }
+
+   return -1;
+}
+
 int cmpfunc(const void*a, const void*b)
 {
     return (*(long int*)a - *(long int*)b);
